47bfe44b2df735a9daa4884d637cb13a3878edf4
StevanCakic <stevancakic93@gmail.com>
2018-02-17 15:06:39 +0100

    improve start game functionality

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -7,9 +7,8 @@ import CannonPipe from "./CannonPipe";
 import CannonBall from "./CannonBall";
 import CurrentScore from "./CurrentScore";
 import FlyingObject from "./FlyingObject";
-import Heart from "./Heart";
-import StartGame from './StartGame';
-import Title from './Title';
+import StartGame from "./StartGame";
+import Title from "./Title";
 
 const Canvas = props => {
   const gameHeight = 1200;
@@ -37,18 +36,31 @@ const Canvas = props => {
       <CannonBase />
       <CannonBall position={{ x: 0, y: -100 }} />
       <CurrentScore score={15} />
-      <FlyingObject position={{ x: -150, y: -300 }} />
-      <FlyingObject position={{ x: 150, y: -300 }} />
-      <Heart position={{ x: -300, y: 35 }} />
-      <StartGame onClick={() => console.log('Aliens, Go Home!')} />
-      <Title />
+      {!props.gameState.started && (
+        <g>
+          <StartGame onClick={() => props.startGame()} />
+          <Title />
+        </g>
+      )}
+      {props.gameState.started && (
+        <g>
+          <FlyingObject position={{ x: -150, y: -300 }} />
+          <FlyingObject position={{ x: 150, y: -300 }} />
+        </g>
+      )}
     </svg>
   );
 };
 
 Canvas.propTypes = {
   angle: PropTypes.number.isRequired,
-  trackMouse: PropTypes.func.isRequired
+  gameState: PropTypes.shape({
+    started: PropTypes.bool.isRequired,
+    kills: PropTypes.number.isRequired,
+    liver: PropTypes.number.isRequired
+  }).isRequired,
+  trackMouse: PropTypes.func.isRequired,
+  startGame: PropTypes.func.isRequired
 };
 
 export default Canvas;
